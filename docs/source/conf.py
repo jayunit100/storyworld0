9b9e4a0ca77c1b796fb77958f8e29027b65a7e26
jay vyas <jayunit100.github@gmail.com>
2023-08-13 00:25:03 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.autosummary'
+    'sphinx.ext.autosummary',
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
